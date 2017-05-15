fbfeb1505cf40d3ec514b8372d3d90a1f45923b2
David L. King <king.david.lee@gmail.com>
2017-05-14 22:17:53 -0400

    added static root and cleaned up

--- w2v_demo/settings.py
+++ w2v_demo/settings.py
@@ -98,5 +98,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 # change to exact path in production
 STATICFILES_DIRS = ( os.path.join(BASE_DIR, "static"), )

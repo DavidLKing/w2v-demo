aef4d2168b16861480377f1eeb77472fe87e6e48
David L. King <king.david.lee@gmail.com>
2017-06-01 21:01:56 -0400

    heroku still not working, ssl redirect off

--- w2v_demo/settings.py
+++ w2v_demo/settings.py
@@ -93,6 +93,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+# SSL support
+SECURE_SSL_REDIRECT = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
